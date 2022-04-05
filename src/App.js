207d2b491e866a16164320848af557f7e3b4dd7e
Jrmart12 <jr.martinez@codex.academy>
2022-04-05 11:13:02 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { utils, colors } from "./utils/utils";
+import React, { useState } from "react";
 
 function App() {
+  const [stars, setStarts] = useState(utils.random(1, 9));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {utils.range(1, stars).map((starId) => (
+            <div key={starId} className="star"></div>
+          ))}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map((number) => (
+            <button key={number} className="number">
+              {number}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>
   );
 }
