1f2d04d512a04c96de94f130d99e3e75e5f8994b
Jrmart12 <jr.martinez@codex.academy>
2022-04-05 12:30:04 -0600

    fixing issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { utils, colors } from "./utils/utils";
 import PlayNumber from "./components/PlayNumber";
@@ -9,9 +9,20 @@ function App() {
   const [stars, setStars] = useState(utils.random(1, 9));
   const [availabeNums, setavailabeNums] = useState(utils.range(1, 9));
   const [candidateNums, setcandidateNums] = useState([]);
+  const [secondsLeft, setsecondsLeft] = useState(10);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availabeNums.length > 0) {
+      const timerId = setTimeout(() => {
+        setsecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  }, [secondsLeft, availabeNums]);
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
-  const gameIsDone = availabeNums.length === 0;
+  const gameStatus =
+    availabeNums.length === 0 ? "won" : secondsLeft === 0 ? "lost" : "active";
 
   const resetGame = () => {
     setStars(utils.random(1, 9));
@@ -30,7 +41,7 @@ function App() {
   };
 
   const onNumberClick = (number, currentStatus) => {
-    if (currentStatus === "used") {
+    if (gameStatus !== "active" || currentStatus === "used") {
       return;
     }
     const newCandidatesNums =
@@ -57,8 +68,8 @@ function App() {
       </div>
       <div className="body">
         <div className="left">
-          {gameIsDone ? (
-            <PlayAgain onClick={resetGame} />
+          {gameStatus !== "active" ? (
+            <PlayAgain onClick={resetGame} gameStatus={gameStatus} />
           ) : (
             <StarsDisplay count={stars} />
           )}
@@ -74,7 +85,7 @@ function App() {
           ))}
         </div>
       </div>
-      <div className="timer">Time Remaining: 10</div>
+      <div className="timer">Time Remaining:{secondsLeft}</div>
     </div>
   );
 }
